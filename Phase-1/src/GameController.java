b13f7892679a9a42ac5fc677a064dc3801d0c1e4
apainintheneck <apainintheneck@gmail.com>
2020-04-09 16:14:48 -0700

    Created and updated GameModel.java.

--- Phase-1/src/GameController.java
+++ Phase-1/src/GameController.java
@@ -1,5 +1,5 @@
-
+//Phase 1 Controller Class for Low Card Game
 public class GameController
 {
-
+   
 }
--- Phase-1/src/GameModel.java
+++ Phase-1/src/GameModel.java
@@ -1,5 +1,23 @@
-
+//Phase 1 Model Class for Low Card Game
 public class GameModel
 {
-
+   CardGameFramework LowCardGame;
+   GUICard cardIcons = new GUICard();
+   
+   GameModel(int numPacksPerDeck, int numJokersPerPack, int numUnusedCardsPerPack, 
+         Card[] unusedCardsPerPack, int numPlayers, int numCardsPerHand) {
+      
+      LowCardGame = new CardGameFramework(numPacksPerDeck, numJokersPerPack, 
+            numUnusedCardsPerPack, unusedCardsPerPack, numPlayers, numCardsPerHand);
+   }
+   
+   //Getters and setters from CardGameFramework
+   public boolean dealNewHands() { return LowCardGame.deal(); }
+   public void sortHands() { LowCardGame.sortHands(); }
+   public Hand getHand(int handNum) { return LowCardGame.getHand(handNum); }
+   public int getNumCardInDeck() { return LowCardGame.getNumCardsRemainingInDeck(); }
+   public Card playCard(int playerIndex, int cardIndex) {
+      return LowCardGame.playCard(playerIndex, cardIndex);
+   }
+   public boolean takeCard(int playerIndex) { return LowCardGame.takeCard(playerIndex); }
 }
